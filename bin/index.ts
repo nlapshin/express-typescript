54a50d63e088faae8ed602b7f13cff79c84078ab
Nikolay Lapshin <nlapshin1989@gmail.com>
2022-03-16 23:33:17 +0300

    Simple server

---
+++ bin/index.ts
@@ -0,0 +1,5 @@
+import { Server } from '../src'
+
+const server = new Server()
+
+server.start()
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import * as http from "http";
+import express, { Express } from "express";
+import bodyParser from "body-parser";
+
+export class Server {
+    private readonly _app: Express;
+
+    get app(): Express {
+        return this._app;
+    }
+
+    private _server!: http.Server;
+
+    get server(): http.Server {
+        return this._server;
+    }
+
+    constructor() {
+        this._app = express();
+
+        this._app.set("port", process.env.PORT || 3000);
+
+        this.configureMiddleware();
+    }
+
+    public configureMiddleware() {
+        this._app.use(bodyParser.json());
+        this._app.use(bodyParser.urlencoded({ extended: true }));
+    }
+
+    public start() {
+        this._server = this._app.listen(this._app.get("port"), () => {
+            console.log("🚀 Server is running on port " + this._app.get("port"));
+        });
+    }
+}
+
+export default Server;
